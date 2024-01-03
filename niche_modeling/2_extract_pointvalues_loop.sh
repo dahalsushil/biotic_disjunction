fdefc19b2b9a3d0066af039e39b824b93db661f8
sd1740 <sd1740@msstate.edu>
2024-01-03 15:15:18 -0600

    .

--- niche_modeling/2_extract_pointvalues_loop.sh
+++ niche_modeling/2_extract_pointvalues_loop.sh
@@ -1,5 +1,5 @@
 # This one drops only the missing values for each point, so point associations are lost
 mkdir pnos_directsampling_no_missing_data_no_point_associations
-for i in ./../../../../nitfix/Saxifragales_all_layers_30s/*.asc; do
+for i in ./../../../nitfix/Saxifragales_all_layers_30s/*.asc; do
 ./extract_pointvalues_evenhigherthroughput.py ${i} ./powo_cleaned/
 done
